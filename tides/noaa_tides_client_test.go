babb7ab8fd4bd5b3fc1acb9aedf4fa1a44dc0412
Mike Weber <msweber@gmail.com>
2021-01-19 19:23:13 -0800

    Use map for census postcode list, and use fake client in tides client test.

--- tides/noaa_tides_client_test.go
+++ tides/noaa_tides_client_test.go
@@ -1,19 +1,61 @@
 package tides_test
 
 import (
+	"net/http"
 	"testing"
 
+	"github.com/webercoder/gocean/testutils"
 	. "github.com/webercoder/gocean/tides"
 )
 
+const NOAAPredictionsJSONData = `{
+    "predictions": [{
+        "t": "2021-01-19 07:00",
+        "v": "1.894"
+    }, {
+        "t": "2021-01-19 07:06",
+        "v": "1.888"
+    }, {
+        "t": "2021-01-19 07:12",
+        "v": "1.882"
+    }, {
+        "t": "2021-01-19 07:18",
+        "v": "1.877"
+    }, {
+        "t": "2021-01-19 07:24",
+        "v": "1.873"
+    }]
+}`
+
+type FakePredictionsClient struct {
+	Err      error
+	JsonData string
+}
+
+func (fsc *FakePredictionsClient) Get(url string) (resp *http.Response, err error) {
+	if fsc.Err != nil {
+		return nil, fsc.Err
+	}
+
+	return &http.Response{
+		Body: testutils.NewStringReadCloser(fsc.JsonData),
+	}, nil
+}
+
 func TestNOAATidesClient_RetrievePredictions(t *testing.T) {
 	station := "9410170"
-	client := NewNOAATidesClient()
+	client := &NOAATidesClient{Client: &FakePredictionsClient{JsonData: NOAAPredictionsJSONData}}
 	data, err := client.RetrievePredictions(station, 1)
 	if err != nil {
 		t.Error("Did not expect error when retrieving tide data", err)
 	}
-	if len(data) == 0 {
+	if len(data) != 5 {
 		t.Error("Zero tides received", data)
+	}
+	if data[2].Time != "2021-01-19 07:12" {
+		t.Error("Unexpected time for the third entry")
+	}
+	if data[2].Value != "1.882" {
+		t.Error("Unexpected value for the third entry")
 	}
 }
--- utils/census_zip_coords.go
+++ utils/census_zip_coords.go
@@ -1,55 +1,13 @@
 package utils
 
 import (
-	"encoding/csv"
 	"fmt"
-	"io"
-	"strconv"
-	"strings"
 )
 
 // FindCoordsForPostcode returns the geocoordinates for a zip code.
 func FindCoordsForPostcode(zip string) (*GeoCoordinates, error) {
-	// Uncomment to read the zip codes from a file and stream search them:
-	// appPath, err := os.Executable()
-	// if err != nil {
-	// 	fmt.Printf("Could not find executable path for loading census file: %v\n", err)
-	// 	return nil, err
-	// }
-	// path := filepath.Join(filepath.Dir(appPath), "data/census-2019-zip-coords.txt")
-	// file, err := os.Open(path)
-	// if err != nil {
-	// 	fmt.Printf("Could not open census data file: %v\n", err)
-	// 	return nil, err
-	// }
-	// defer file.Close()
-	// parser := csv.NewReader(file)
-
-	parser := csv.NewReader(strings.NewReader(CensusZipCoords))
-	parser.Comma = '\t'
-
-	for {
-		record, err := parser.Read()
-		if err == io.EOF {
-			break
-		}
-
-		if zip == record[0] {
-			lat, err := strconv.ParseFloat(strings.TrimSpace(record[5]), 64)
-			if err != nil {
-				return nil, fmt.Errorf("Found zip but could not parse latitude from Census data")
-			}
-
-			long, err := strconv.ParseFloat(strings.TrimSpace(record[6]), 64)
-			if err != nil {
-				return nil, fmt.Errorf("Found zip but could not parse longitude from Census data")
-			}
-
-			return &GeoCoordinates{
-				Lat:  lat,
-				Long: long,
-			}, nil
-		}
+	if coords, ok := CensusZipCoords[zip]; ok {
+		return &coords, nil
 	}
 
 	return nil, fmt.Errorf("Zip %s not found", zip)
